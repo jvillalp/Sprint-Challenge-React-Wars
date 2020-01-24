05fbda7c3aa74bf40d096fb3f5e9156d134454a8
Juana Villalpando <juana.v14@gmail.com>
2020-01-24 10:56:36 -0800

    answered questions

--- starwars/src/components/Body/Body.js
+++ starwars/src/components/Body/Body.js
@@ -3,41 +3,44 @@ import React from "react";
 import styled from "styled-components";
 import {
   Card,
-  Button,
   CardHeader,
-  CardFooter,
   CardBody,
   CardText,
   Col
 } from "reactstrap";
 
-const Navstyle = styled.nav`
+const Cardstyle = styled.nav`
 background: #6B8E23;
 color: white;
-padding: 2%;
+padding: 1%;
 display: flex;
 align-items: center;
 flex-wrap: wrap;
 `;
 
+const CardChar = styled.div`
+display:flex
+flex-direction: column;
+align-items: center;
+justify-content: space-between;
+margin-bottom: 20px;
+`;
+
 const Body = props => {
   return (
       
-    <Col xs="6" md="4" xl="2">
+    <Col xs="12" md="6" xl="3">
 
-      <Card body outline color="success">
-          <Navstyle>
+      <Card CardChar body outline color="success">
+          <Cardstyle>
         <CardHeader>{props.name}</CardHeader>
         <CardBody>
           <CardText>Gender: {props.gender}</CardText>
           <CardText>Height: {props.height} cm</CardText>
-          <CardText>Mass {props.mass}</CardText>
+          <CardText>Mass: {props.mass} kg</CardText>
           <CardText>Birth Year: {props.birthYear}</CardText>
-          <CardFooter>
-            <Button>Learn More</Button>
-          </CardFooter>
         </CardBody>
-        </Navstyle>
+        </Cardstyle>
       </Card>
     </Col>
    

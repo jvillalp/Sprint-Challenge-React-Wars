18fdbf17453e20151cd31cbabae90119e960436f
Juana Villalpando <juana.v14@gmail.com>
2020-01-24 11:41:10 -0800

    update cards

--- starwars/src/components/Body/Body.js
+++ starwars/src/components/Body/Body.js
@@ -4,26 +4,27 @@ import styled from "styled-components";
 import { Card, CardHeader, CardBody, CardText, Col } from "reactstrap";
 
 const Cardstyle = styled.nav`
-  background: #6b8e23;
-  color: white;
-  padding: 2%;
+  // background: #6b8e23;
+  // color: white;
+  border: 2px dotted white;
+  padding: 1%;
   display: flex;
   align-items: center;
   flex-wrap: wrap;
 `;
 
-const CardChar = styled.div`
-  display:flex
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  margin-bottom: 20px;
-`;
+// const CardChar = styled.div`
+//   display:flex
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: space-between;
+//   margin-bottom: 50%;
+// `;
 
 const Body = props => {
   return (
     <Col xs="12" md="6" xl="3">
-      <Card CardChar body outline color="success">
+      <Card body className="text-center" outline color="danger" inverse style={{ backgroundColor: '#2E8B57', borderColor: 'FFF8DC', opacity: 0.7}}>
         <Cardstyle>
           <CardHeader>{props.name}</CardHeader>
           <CardBody>
